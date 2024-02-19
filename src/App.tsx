bfb76d1312e0a04542182be4ff55035e930d15ca
rei-R2 <reifaldi.r2@gmail.com>
2024-02-19 23:55:16 +0700

    feat: add Itinerary and Photos components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import HomePage from "./pages/home";
 import { ReactLenis } from "@studio-freight/react-lenis";
 import OurStory from "./pages/ourStory";
+import Itinerary from "./pages/itinerary";
+import Photos from "./pages/photos";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
       <div className="flex flex-col gap-y-40">
         <HomePage />
         <OurStory />
+        <Itinerary />
+        <Photos />
       </div>
     </ReactLenis>
   );
